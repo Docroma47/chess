590b23b700aa96d2c6d3f215a835fa49b870f983
Docroma47 <mom47@yandex.ru>
2017-07-25 07:31:54 +0300

    fix for

--- src/main/java/org/mom47/chess/App.java
+++ src/main/java/org/mom47/chess/App.java
@@ -5,77 +5,46 @@ import org.fusesource.jansi.AnsiConsole;
 import org.mom47.chess.model.*;
 
 public class App {
-
     public static void main(String[] args) {
         ChessPiece[] figures = new ChessPiece[32];
         AnsiConsole.systemInstall();
         printChessDesk(1, 1);
 
-        figures[0] = new King(PieceColour.WHITE, 5);
+        figures[16] = new King(PieceColour.WHITE, 5);
 
-        figures[1] = new Queen(PieceColour.WHITE, 4);
+        figures[17] = new Queen(PieceColour.WHITE, 4);
 
-        figures[2] = new Bishop(PieceColour.WHITE, 6);
+        figures[18] = new Bishop(PieceColour.WHITE, 6);
 
-        figures[3] = new Bishop(PieceColour.WHITE, 3);
+        figures[19] = new Bishop(PieceColour.WHITE, 3);
 
-        figures[4] = new Knight(PieceColour.WHITE, 7);
+        figures[20] = new Knight(PieceColour.WHITE, 7);
 
-        figures[5] = new Knight(PieceColour.WHITE, 2);
+        figures[21] = new Knight(PieceColour.WHITE, 2);
 
-        figures[6] = new Rook(PieceColour.WHITE, 8);
+        figures[22] = new Rook(PieceColour.WHITE, 8);
 
-        figures[7] = new Rook(PieceColour.WHITE, 1);
+        figures[23] = new Rook(PieceColour.WHITE, 1);
 
-        figures[8] = new Pawn(PieceColour.WHITE, 1);
+        figures[24] = new King(PieceColour.BLACK, 5);
 
-        figures[9] = new Pawn(PieceColour.WHITE,2);
+        figures[25] = new Queen(PieceColour.BLACK, 4);
 
-        figures[10] = new Pawn(PieceColour.WHITE, 3);
+        figures[26] = new Bishop(PieceColour.BLACK, 6);
 
-        figures[11] = new Pawn(PieceColour.WHITE, 4);
+        figures[27] = new Bishop(PieceColour.BLACK, 3);
 
-        figures[12] = new Pawn(PieceColour.WHITE, 5);
+        figures[28] = new Knight(PieceColour.BLACK, 7);
 
-        figures[13] = new Pawn(PieceColour.WHITE, 6);
+        figures[29] = new Knight(PieceColour.BLACK, 2);
 
-        figures[14] = new Pawn(PieceColour.WHITE, 7);
+        figures[30] = new Rook(PieceColour.BLACK, 8);
 
-        figures[15] = new Pawn(PieceColour.WHITE, 8);
+        figures[31] = new Rook(PieceColour.BLACK, 1);
 
-        figures[16] = new King(PieceColour.BLACK, 5);
-
-        figures[17] = new Queen(PieceColour.BLACK, 4);
-
-        figures[18] = new Bishop(PieceColour.BLACK, 6);
-
-        figures[19] = new Bishop(PieceColour.BLACK, 3);
-
-        figures[20] = new Knight(PieceColour.BLACK, 7);
-
-        figures[21] = new Knight(PieceColour.BLACK, 2);
-
-        figures[22] = new Rook(PieceColour.BLACK, 8);
-
-        figures[23] = new Rook(PieceColour.BLACK, 1);
-
-        figures[24] = new Pawn(PieceColour.BLACK, 1);
-
-        figures[25] = new Pawn(PieceColour.BLACK, 2);
-
-        figures[26] = new Pawn(PieceColour.BLACK, 3);
-
-        figures[27] = new Pawn(PieceColour.BLACK, 4);
-
-        figures[28] = new Pawn(PieceColour.BLACK, 5);
-
-        figures[29] = new Pawn(PieceColour.BLACK, 6);
-
-        figures[30] = new Pawn(PieceColour.BLACK, 7);
-
-        figures[31] = new Pawn(PieceColour.BLACK, 8);
-        for (int i = 0; i <= 31; i++) {
-            print(figures[i]);
+        for (int i = 0; i <= 7; i++) {
+            print(figures[i] = new Pawn(PieceColour.WHITE, 1 + i));
+            print(figures[i + 7] = new Pawn(PieceColour.BLACK, 1 + i));
         }
 
         AnsiConsole.systemUninstall();
