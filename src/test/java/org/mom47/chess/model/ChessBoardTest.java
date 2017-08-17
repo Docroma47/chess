6ba4bd60b58977acd003a6eecbb6ffb5355842a0
Vlad Kolotov <vkolotov@gmail.com>
2017-08-18 07:51:41 +1200

    Adding a very new test for ChessBoard

---
+++ src/test/java/org/mom47/chess/model/ChessBoardTest.java
@@ -0,0 +1,23 @@
+package org.mom47.chess.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ChessBoardTest {
+
+    @Test
+    public void testGetFreeSquires() throws Exception {
+        ChessBoard chessBoard = new ChessBoard();
+
+        assertEquals(63, chessBoard.getFreeSquires());
+
+        King king = new King(PieceColour.BLACK, 0);
+        chessBoard.setPiece(king);
+        assertEquals(63, chessBoard.getFreeSquires());
+
+        Queen queen = new Queen(PieceColour.WHITE, 7);
+        chessBoard.setPiece(queen);
+        assertEquals(62, chessBoard.getFreeSquires());
+    }
+}
