89ca4990514ed00b4894e49cce98615b7f1aebef
Vlad Kolotov <vkolotov@gmail.com>
2017-08-18 08:16:42 +1200

    minor changes to make the code more readable for the student

--- src/test/java/org/mom47/chess/model/ChessBoardTest.java
+++ src/test/java/org/mom47/chess/model/ChessBoardTest.java
@@ -1,8 +1,7 @@
 package org.mom47.chess.model;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class ChessBoardTest {
 
@@ -10,14 +9,14 @@ public class ChessBoardTest {
     public void testGetFreeSquires() throws Exception {
         ChessBoard chessBoard = new ChessBoard();
 
-        assertEquals(64, chessBoard.getFreeSquires());
+        Assert.assertEquals(64, chessBoard.getFreeSquires());
 
         King king = new King(PieceColour.BLACK, 0);
         chessBoard.setPiece(king);
-        assertEquals(63, chessBoard.getFreeSquires());
+        Assert.assertEquals(63, chessBoard.getFreeSquires());
 
         Queen queen = new Queen(PieceColour.WHITE, 7);
         chessBoard.setPiece(queen);
-        assertEquals(62, chessBoard.getFreeSquires());
+        Assert.assertEquals(62, chessBoard.getFreeSquires());
     }
 }
