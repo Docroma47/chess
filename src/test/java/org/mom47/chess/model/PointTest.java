3a0b0d31acadade97012e39f1c6f1910a0358481
Vlad Kolotov <vkolotov@gmail.com>
2017-08-30 16:05:38 +1200

    Adding a new unittest for Point class

---
+++ src/test/java/org/mom47/chess/model/PointTest.java
@@ -0,0 +1,17 @@
+package org.mom47.chess.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void testEquals() {
+        Point first = new Point(1, 1);
+        Point second = new Point(1, 1);
+
+        Assert.assertFalse(first == second);
+        Assert.assertEquals(first, second);
+    }
+
+}
