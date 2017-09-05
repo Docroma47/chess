93e4acb4d266e2de8a41aa3a5726585114dd1896
Docroma47 <mom47@yandex.ru>
2017-09-06 00:50:56 +0300

    bugs v 0.1

--- src/test/java/org/mom47/chess/model/FunControllerTest.java
+++ src/test/java/org/mom47/chess/model/FunControllerTest.java
@@ -1,7 +1,19 @@
 package org.mom47.chess.model;
 
-/**
- * Created by mom47 on 6/9/17.
- */
+import org.junit.Assert;
+import org.junit.Test;
+import org.mom47.chess.controller.FunController;
+import org.mom47.chess.view.ChessBashView;
+
 public class FunControllerTest {
+
+    @Test
+    public void testGetRandomIndexes() {
+        Chess chess = new Chess();
+        ChessBashView chessBashView = new ChessBashView(chess);
+        FunController funController = new FunController(chess, chessBashView);
+        for (int x = 0; x <= 31; x++) {
+            System.out.println(funController.getRandomIndexes(x).length);
+        }
+    }
 }
