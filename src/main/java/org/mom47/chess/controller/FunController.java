4205c5630c15aebcfc002d561a5fb232a5a776ab
Docroma47 <mom47@yandex.ru>
2017-09-08 02:47:19 +0300

    Fix bugs

--- src/main/java/org/mom47/chess/controller/FunController.java
+++ src/main/java/org/mom47/chess/controller/FunController.java
@@ -124,8 +124,11 @@ public class FunController {
 
     public int[] getRandomIndexes(int length) {
         Random random = new Random();
-        int[] indexes = new int[31];
-        for(int i = 1; i < length; i++) {
+        int[] indexes = new int[32];
+        for (int i = 0; i < length; i++) {
+            indexes[i] = i;
+        }
+        for(int i = 0; i < length; i++) {
             int randomIndex = random.nextInt(31);
             int temp = indexes[i];
             indexes[i] = indexes[randomIndex];
