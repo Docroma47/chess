88cce21e91c2892f991b5c867e1c24dd5ff2637b
Vlad Kolotov <vkolotov@gmail.com>
2017-09-08 11:51:46 +1200

    Adding a new check for GetRandomIdnexes test

--- src/test/java/org/mom47/chess/model/FunControllerTest.java
+++ src/test/java/org/mom47/chess/model/FunControllerTest.java
@@ -1,28 +1,29 @@
 package org.mom47.chess.model;
 
-import org.junit.Assert;
+import junit.framework.Assert;
 import org.junit.Test;
 import org.mom47.chess.controller.FunController;
 import org.mom47.chess.view.ChessBashView;
 
 public class FunControllerTest {
     public int variable;
-    public int[] variable1 = new int[10];
+
 
     @Test
     public void testGetRandomIndexes() {
         Chess chess = new Chess();
         ChessBashView chessBashView = new ChessBashView(chess);
         FunController funController = new FunController(chess, chessBashView);
-        int[] x = funController.getRandomIndexes(31);
-        for (int i = 1; i <= 31; i++) {
+        int[] x = funController.getRandomIndexes(64);
+        Assert.assertEquals(64, x.length);
+        for (int i = 1; i <= 64; i++) {
             System.out.println(x[i]);
         }
     }
 
     @Test
     public void testVariable() {
-
+        int[] variable1 = new int[10];
         System.out.println("variable: " + variable);
         for(int i = 0; i <= 9; i++) {
             System.out.println("variable char: " + variable1[i]);
