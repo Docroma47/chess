051eed6330eadfc60b912f2e2d3c5398e4283859
Vlad Kolotov <vkolotov@gmail.com>
2017-09-11 11:47:38 +1200

    adding a test case for King's getAvailablePaths when it sits by the bottom edge

--- src/test/java/org/mom47/chess/model/KingTest.java
+++ src/test/java/org/mom47/chess/model/KingTest.java
@@ -95,5 +95,39 @@ public class KingTest {
 
         // check the path UP-LEFT
         Assert.assertEquals(new Point(4, 5), availablePaths[7][0]);
+
+        // moving the king to the bottom edge ==================
+
+        chessBoard.move(king.getPosition(), new Point(4, 0));
+        position = king.getPosition();
+        Assert.assertEquals(new Point(4, 0), position);
+
+        availablePaths = king.getAvailablePaths();
+        Assert.assertEquals(8, availablePaths.length);
+
+        // check that every paths is 1 squire long
+        Assert.assertEquals(1, availablePaths[0].length);
+        Assert.assertEquals(1, availablePaths[1].length);
+        Assert.assertEquals(1, availablePaths[2].length);
+        Assert.assertEquals(0, availablePaths[3].length);
+        Assert.assertEquals(0, availablePaths[4].length);
+        Assert.assertEquals(0, availablePaths[5].length);
+        Assert.assertEquals(1, availablePaths[6].length);
+        Assert.assertEquals(1, availablePaths[7].length);
+
+        // check the path UP
+        Assert.assertEquals(new Point(4, 1), availablePaths[0][0]);
+
+        // check the path UP-RIGHT
+        Assert.assertEquals(new Point(4, 1), availablePaths[1][0]);
+
+        // check the path RIGHT
+        Assert.assertEquals(new Point(5, 0), availablePaths[2][0]);
+
+        // check the path LEFT
+        Assert.assertEquals(new Point(3, 0), availablePaths[6][0]);
+
+        // check the path UP-LEFT
+        Assert.assertEquals(new Point(3, 1), availablePaths[7][0]);
     }
 }
