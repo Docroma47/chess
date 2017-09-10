01a55e393612c0824276398ac70dc96ab0097daa
Vlad Kolotov <vkolotov@gmail.com>
2017-09-11 11:11:49 +1200

    adding a test case for King's getAvailablePaths

--- src/test/java/org/mom47/chess/model/KingTest.java
+++ src/test/java/org/mom47/chess/model/KingTest.java
@@ -52,5 +52,48 @@ public class KingTest {
 
         // check the path UP-LEFT
         Assert.assertEquals(new Point(3, 5), availablePaths[7][0]);
+
+        // moving the king by one squire to he right ==================
+
+        chessBoard.move(king.getPosition(), new Point(5, 4));
+        position = king.getPosition();
+        Assert.assertEquals(new Point(5, 4), position);
+
+        availablePaths = king.getAvailablePaths();
+        Assert.assertEquals(8, availablePaths.length);
+
+        // check that every paths is 1 squire long
+        Assert.assertEquals(1, availablePaths[0].length);
+        Assert.assertEquals(1, availablePaths[1].length);
+        Assert.assertEquals(1, availablePaths[2].length);
+        Assert.assertEquals(1, availablePaths[3].length);
+        Assert.assertEquals(1, availablePaths[4].length);
+        Assert.assertEquals(1, availablePaths[5].length);
+        Assert.assertEquals(1, availablePaths[6].length);
+        Assert.assertEquals(1, availablePaths[7].length);
+
+        // check the path UP
+        Assert.assertEquals(new Point(5, 5), availablePaths[0][0]);
+
+        // check the path UP-RIGHT
+        Assert.assertEquals(new Point(6, 5), availablePaths[1][0]);
+
+        // check the path RIGHT
+        Assert.assertEquals(new Point(6, 4), availablePaths[2][0]);
+
+        // check the path DOWN-RIGHT
+        Assert.assertEquals(new Point(6, 3), availablePaths[3][0]);
+
+        // check the path DOWN
+        Assert.assertEquals(new Point(5, 3), availablePaths[4][0]);
+
+        // check the path DOWN-LEFT
+        Assert.assertEquals(new Point(4, 3), availablePaths[5][0]);
+
+        // check the path LEFT
+        Assert.assertEquals(new Point(4, 4), availablePaths[6][0]);
+
+        // check the path UP-LEFT
+        Assert.assertEquals(new Point(4, 5), availablePaths[7][0]);
     }
 }
