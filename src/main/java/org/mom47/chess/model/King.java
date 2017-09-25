22f7eb88679756667157d0c287b391563c73055b
Docroma47 <mom47@yandex.ru>
2017-09-25 04:41:49 +0300

    Fix 0 array char

--- src/main/java/org/mom47/chess/model/King.java
+++ src/main/java/org/mom47/chess/model/King.java
@@ -13,63 +13,112 @@ public class King extends ChessPiece {
                 free[0][0] = position.getUp();
                 free[1][0] = position.getUpRight();
                 free[2][0] = position.getRight();
+                free[3] = new Point[]{ };
+                free[4] = new Point[]{ };
+                free[5] = new Point[]{ };
+                free[6] = new Point[]{ };
+                free[7] = new Point[]{ };
             } else if (position.getFile() == 7) {
                 free[0][0] = position.getUp();
+                free[1] = new Point[]{ };
+                free[2] = new Point[]{ };
+                free[3] = new Point[]{ };
+                free[4] = new Point[]{ };
+                free[5] = new Point[]{ };
                 free[6][0] = position.getLeft();
                 free[7][0] = position.getUpLeft();
             } else {
                 free[0][0] = position.getUp();
                 free[1][0] = position.getUpRight();
                 free[2][0] = position.getRight();
-                free[3][0] = //!free[3][0] = new Point[8][0];
-                free[4][0] = //
-                free[5][0] = //
+                free[3] = new Point[]{ };
+                free[4] = new Point[]{ };
+                free[5] = new Point[]{ };
                 free[6][0] = position.getLeft();
                 free[7][0] = position.getUpLeft();
             }
         } else if (position.getRank() == 7) {
             if (position.getFile() == 0) {
+                free[0] = new Point[]{ };
+                free[1] = new Point[]{ };
                 free[2][0] = position.getRight();
                 free[3][0] = position.getDownRight();
                 free[4][0] = position.getDown();
+                free[5] = new Point[]{ };
+                free[6] = new Point[]{ };
+                free[7] = new Point[]{ };
             } else if (position.getFile() == 7) {
+                free[0] = new Point[]{ };
+                free[1] = new Point[]{ };
+                free[2] = new Point[]{ };
+                free[3] = new Point[]{ };
                 free[4][0] = position.getDown();
                 free[5][0] = position.getDownLeft();
                 free[6][0] = position.getLeft();
+                free[7] = new Point[]{ };
             } else {
+                free[0] = new Point[]{ };
+                free[1] = new Point[]{ };
                 free[2][0] = position.getRight();
                 free[3][0] = position.getDownRight();
                 free[4][0] = position.getDown();
                 free[5][0] = position.getDownLeft();
                 free[6][0] = position.getLeft();
+                free[7] = new Point[]{ };
             }
         } else if (position.getFile() == 0) {
             if (position.getRank() == 0) {
                 free[0][0] = position.getUp();
                 free[1][0] = position.getUpRight();
                 free[2][0] = position.getRight();
+                free[3] = new Point[]{ };
+                free[4] = new Point[]{ };
+                free[5] = new Point[]{ };
+                free[6] = new Point[]{ };
+                free[7] = new Point[]{ };
             } else if (position.getRank() == 7) {
+                free[0] = new Point[]{ };
+                free[1] = new Point[]{ };
                 free[2][0] = position.getRight();
                 free[3][0] = position.getDownRight();
                 free[4][0] = position.getDown();
+                free[5] = new Point[]{ };
+                free[6] = new Point[]{ };
+                free[7] = new Point[]{ };
             } else {
                 free[0][0] = position.getUp();
                 free[1][0] = position.getUpRight();
                 free[2][0] = position.getRight();
                 free[3][0] = position.getDownRight();
                 free[4][0] = position.getDown();
+                free[5] = new Point[]{ };
+                free[6] = new Point[]{ };
+                free[7] = new Point[]{ };
             }
         } else if (position.getFile() == 7) {
             if (position.getRank() == 7) {
+                free[0] = new Point[]{ };
+                free[1] = new Point[]{ };
+                free[2] = new Point[]{ };
+                free[3] = new Point[]{ };
                 free[4][0] = position.getDown();
                 free[5][0] = position.getDownLeft();
                 free[6][0] = position.getLeft();
+                free[7] = new Point[]{ };
             } else if (position.getRank() == 0) {
                 free[0][0] = position.getUp();
+                free[1] = new Point[]{ };
+                free[2] = new Point[]{ };
+                free[3] = new Point[]{ };
+                free[4] = new Point[]{ };
+                free[5] = new Point[]{ };
                 free[6][0] = position.getLeft();
                 free[7][0] = position.getUpLeft();
             } else {
                 free[0][0] = position.getUp();
+                free[1] = new Point[]{ };
+                free[2] = new Point[]{ };
+                free[3] = new Point[]{ };
                 free[4][0] = position.getDown();
                 free[5][0] = position.getDownLeft();
                 free[6][0] = position.getLeft();
