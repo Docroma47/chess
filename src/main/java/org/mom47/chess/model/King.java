074699485b434182502be0883d4fe70843df5b38
Docroma47 <mom47@yandex.ru>
2017-10-08 11:20:17 +1300

    change king №1

--- src/main/java/org/mom47/chess/model/King.java
+++ src/main/java/org/mom47/chess/model/King.java
@@ -8,11 +8,16 @@ public class King extends ChessPiece {
     public Point[][] getAvailablePaths() {
         Point[][] free = new Point[8][1];
         Point position = getPosition();
+        free[0][0] = position.getUp();
+        free[1][0] = position.getUpRight();
+        free[2][0] = position.getRight();
+        free[3][0] = position.getDownRight();
+        free[4][0] = position.getDown();
+        free[5][0] = position.getDownLeft();
+        free[6][0] = position.getLeft();
+        free[7][0] = position.getUpLeft();
         if (position.getRank() == 0) {
             if (position.getFile() == 0) {
-                free[0][0] = position.getUp();
-                free[1][0] = position.getUpRight();
-                free[2][0] = position.getRight();
                 free[3] = new Point[]{ };
                 free[4] = new Point[]{ };
                 free[5] = new Point[]{ };
@@ -25,25 +30,15 @@ public class King extends ChessPiece {
                 free[3] = new Point[]{ };
                 free[4] = new Point[]{ };
                 free[5] = new Point[]{ };
-                free[6][0] = position.getLeft();
-                free[7][0] = position.getUpLeft();
             } else {
-                free[0][0] = position.getUp();
-                free[1][0] = position.getUpRight();
-                free[2][0] = position.getRight();
                 free[3] = new Point[]{ };
                 free[4] = new Point[]{ };
                 free[5] = new Point[]{ };
-                free[6][0] = position.getLeft();
-                free[7][0] = position.getUpLeft();
             }
         } else if (position.getRank() == 7) {
             if (position.getFile() == 0) {
                 free[0] = new Point[]{ };
                 free[1] = new Point[]{ };
-                free[2][0] = position.getRight();
-                free[3][0] = position.getDownRight();
-                free[4][0] = position.getDown();
                 free[5] = new Point[]{ };
                 free[6] = new Point[]{ };
                 free[7] = new Point[]{ };
@@ -52,25 +47,14 @@ public class King extends ChessPiece {
                 free[1] = new Point[]{ };
                 free[2] = new Point[]{ };
                 free[3] = new Point[]{ };
-                free[4][0] = position.getDown();
-                free[5][0] = position.getDownLeft();
-                free[6][0] = position.getLeft();
                 free[7] = new Point[]{ };
             } else {
                 free[0] = new Point[]{ };
                 free[1] = new Point[]{ };
-                free[2][0] = position.getRight();
-                free[3][0] = position.getDownRight();
-                free[4][0] = position.getDown();
-                free[5][0] = position.getDownLeft();
-                free[6][0] = position.getLeft();
                 free[7] = new Point[]{ };
             }
         } else if (position.getFile() == 0) {
             if (position.getRank() == 0) {
-                free[0][0] = position.getUp();
-                free[1][0] = position.getUpRight();
-                free[2][0] = position.getRight();
                 free[3] = new Point[]{ };
                 free[4] = new Point[]{ };
                 free[5] = new Point[]{ };
@@ -79,18 +63,10 @@ public class King extends ChessPiece {
             } else if (position.getRank() == 7) {
                 free[0] = new Point[]{ };
                 free[1] = new Point[]{ };
-                free[2][0] = position.getRight();
-                free[3][0] = position.getDownRight();
-                free[4][0] = position.getDown();
                 free[5] = new Point[]{ };
                 free[6] = new Point[]{ };
                 free[7] = new Point[]{ };
             } else {
-                free[0][0] = position.getUp();
-                free[1][0] = position.getUpRight();
-                free[2][0] = position.getRight();
-                free[3][0] = position.getDownRight();
-                free[4][0] = position.getDown();
                 free[5] = new Point[]{ };
                 free[6] = new Point[]{ };
                 free[7] = new Point[]{ };
@@ -101,9 +77,6 @@ public class King extends ChessPiece {
                 free[1] = new Point[]{ };
                 free[2] = new Point[]{ };
                 free[3] = new Point[]{ };
-                free[4][0] = position.getDown();
-                free[5][0] = position.getDownLeft();
-                free[6][0] = position.getLeft();
                 free[7] = new Point[]{ };
             } else if (position.getRank() == 0) {
                 free[0][0] = position.getUp();
@@ -112,27 +85,12 @@ public class King extends ChessPiece {
                 free[3] = new Point[]{ };
                 free[4] = new Point[]{ };
                 free[5] = new Point[]{ };
-                free[6][0] = position.getLeft();
-                free[7][0] = position.getUpLeft();
             } else {
                 free[0][0] = position.getUp();
                 free[1] = new Point[]{ };
                 free[2] = new Point[]{ };
                 free[3] = new Point[]{ };
-                free[4][0] = position.getDown();
-                free[5][0] = position.getDownLeft();
-                free[6][0] = position.getLeft();
-                free[7][0] = position.getUpLeft();
             }
-        } else {
-            free[0][0] = position.getUp();
-            free[1][0] = position.getUpRight();
-            free[2][0] = position.getRight();
-            free[3][0] = position.getDownRight();
-            free[4][0] = position.getDown();
-            free[5][0] = position.getDownLeft();
-            free[6][0] = position.getLeft();
-            free[7][0] = position.getUpLeft();
         }
         return free;
     }
