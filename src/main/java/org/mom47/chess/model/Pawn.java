3644c9030a30f4d35c5893b18da6f53743f0261c
Docroma47 <mom47@yandex.ru>
2017-11-05 08:13:22 +0300

    Add more logic Pawn

--- src/main/java/org/mom47/chess/model/Pawn.java
+++ src/main/java/org/mom47/chess/model/Pawn.java
@@ -1,21 +1,44 @@
 package org.mom47.chess.model;
 
 
-import java.util.Arrays;
 
 public class Pawn extends ChessPiece {
     private static final String WHITE_PAWN = "♟";
     private static final String BLACK_PAWN = "♙";
     @Override
     public Point[][] getAvailablePaths(ChessBoard chessBoard) {
-        Point[][] free = new Point[1][1];
+        Point[][] free = new Point[3][1];
         Point position = getPosition();
         if (getColor() == PieceColour.WHITE) {
-            free[0] = getClearPath(chessBoard, position.getUpPath(1));
+            if (position.getRank() != 1) {
+                free[0] = getClearPath(chessBoard, position.getUpPath(1));
+            } else if (position.getRank() == 1) {
+                free[0] = getClearPath(chessBoard, position.getUpPath(2));
+            }
+            free[1] = getClearPath(chessBoard, position.getUpRightPath(1));
+            free[2] = getClearPath(chessBoard, position.getUpLeftPath(1));
+            if (free[0].length != 0 && chessBoard.getPiece(free[0][0]) != null) {
+                free[0] = new Point[] { };
+            }
         }
 
         if (getColor() == PieceColour.BLACK) {
-            free[0] = getClearPath(chessBoard, position.getDownPath(1));
+            if (position.getRank() != 6) {
+                free[0] = getClearPath(chessBoard, position.getDownPath(1));
+            } else if (position.getRank() == 6) {
+                free[0] = getClearPath(chessBoard, position.getDownPath(2));
+            }
+            free[1] = getClearPath(chessBoard, position.getDownRightPath(1));
+            free[2] = getClearPath(chessBoard, position.getDownLeftPath(1));
+        }
+
+        if (free[1].length != 0 && chessBoard.getPiece(free[1][0]) == null || free[2].length != 0 && chessBoard.getPiece(free[2][0]) == null) {
+            if (free[1].length != 0 && chessBoard.getPiece(free[1][0]) == null) {
+                free[1] = new Point[] { };
+            }
+            if (free[2].length != 0 && chessBoard.getPiece(free[2][0]) == null) {
+                free[2] = new Point[] { };
+            }
         }
 
         return free;
